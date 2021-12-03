b14cb3e41fc6a3b1cdf71ffa3e3f9d376b7f9776
Oliver Irwin <oliver.irwin.etu@univ-lille.fr>
2021-12-04 00:10:47 +0100

    feat : update files to use new architecture

--- Projet-Mesa/boxes/model.py
+++ Projet-Mesa/boxes/model.py
@@ -4,6 +4,9 @@ from mesa.space import MultiGrid
 from mesa.datacollection import DataCollector
 from collectors import get_nb_carrying, get_remaining_boxes
 
+from agents.dummy_agent import DummyAgent
+from agents.props import Box, Destination
+
 
 class GridModel(Model) :
     def __init__(self,
@@ -12,70 +15,6 @@ class GridModel(Model) :
                  torus : bool) :
         super(GridModel, self).__init__()
         self.grid = MultiGrid(width, height, torus)
-
-
-class BoxAgent(Agent) :
-    def __init__(self,
-                 unique_id : int,
-                 model : GridModel,
-                 weight : int) :
-        super(BoxAgent, self).__init__(unique_id, model)
-        self.weight = weight
-
-
-class CarrierAgent(Agent) :
-
-    def __init__(self,
-                 unique_id : int,
-                 model : GridModel,
-                 strength : int) :
-        super(CarrierAgent, self).__init__(unique_id, model)
-        self.carrying = False
-        self.target_box = None
-        self.strength = strength
-        self.currently_carrying = 0
-
-    def move(self) :
-        possible_steps = self.model.grid.get_neighborhood(
-                self.pos, moore = True, include_center = False
-        )
-        if (self.carrying) :
-            new_position = min(possible_steps)
-        else :
-            new_position = self.random.choice(possible_steps)
-        self.model.grid.move_agent(self, new_position)
-
-    def pickup_box(self, box : BoxAgent) :
-        print(f"Box : w -> {box.weight}, id -> {box.unique_id}")
-        self.carrying = True
-        self.currently_carrying += box.weight
-        # del self.model.boxes[box.unique_id]
-        self.model.schedule.remove(box)
-        self.model.grid.remove_agent(box)
-
-    def drop_box(self) :
-        print(f"Agent dropped box of weight {self.currently_carrying}")
-        self.carrying = False
-        self.currently_carrying = 0
-
-    def step(self) :
-        self.move()
-        cellmates = self.model.grid.get_cell_list_contents([self.pos])
-        boxes = [agent for agent in cellmates if isinstance(agent, BoxAgent)]
-        #print(boxes)
-        if len(boxes) > 0 :
-            self.pickup_box(boxes[0])
-
-        if self.pos == (0, 0) :
-            self.drop_box()
-
-
-class DestinationAgent(Agent) :
-    def __init__(self, 
-                 unique_id : int, 
-                 model : GridModel) :
-        super(DestinationAgent, self).__init__(unique_id, model)
-
 
 
 class BoxModel(GridModel) :
@@ -95,14 +34,14 @@ class BoxModel(GridModel) :
 
         # Populate
         for i in range(self.nb_agents) :
-            agent = CarrierAgent(i, self, 10)
+            agent = DummyAgent(i, self, strength = 10)
             self.schedule.add(agent)
             self.carriers[i] = agent
             self.grid.place_agent(agent, (0, 0))
 
         for i in range(self.nb_boxes) :
             index = i + self.nb_agents
-            box = BoxAgent(index, self, 5)
+            box = Box(index, self)
 
             self.schedule.add(box)
             self.boxes[index] = box
@@ -112,7 +51,7 @@ class BoxModel(GridModel) :
 
             self.grid.place_agent(box, (x, y))
 
-        dest = DestinationAgent(nb_agents + nb_boxes, self)
+        dest = Destination(nb_agents + nb_boxes, self)
         self.schedule.add(dest)
         self.grid.place_agent(dest, (0, 0))
 
--- Projet-Mesa/boxes/visualiser.py
+++ Projet-Mesa/boxes/visualiser.py
@@ -5,58 +5,18 @@ from mesa.visualization.modules import CanvasGrid
 from mesa.visualization.ModularVisualization import ModularServer
 from mesa.visualization.modules import ChartModule
 from mesa.visualization.ModularVisualization import VisualizationElement
+from typing import Dict, Union
+
+from agents.portrayed_agent import PortrayedAgent
 
 
-def agent_portrayal(agent : Agent) :
-    if isinstance(agent, BoxAgent) :
-        return box_portrayal(agent)
-    elif isinstance(agent, CarrierAgent) :
-        return carrier_portrayal(agent)
-    return destination()
+def agent_portrayal(agent : PortrayedAgent) -> Dict[str, Union[str, float]]:
+    return agent.portray()
 
 
-def carrier_portrayal(agent : CarrierAgent) :
-    portrayal = {
-        "Shape"  : "rect",
-        "Filled" : "true",
-        "w"      : 0.8,
-        "h"      : 0.8,
-        "Layer"  : 0
-    }
-    if not agent.carrying :
-        color = "#A0B8CF"
-    else :
-        color = "#EFAD06"
-    portrayal["Color"] = color
-    return portrayal
-
-
-def box_portrayal(box : BoxAgent) :
-    portrayal = {
-        "Shape"  : "rect",
-        "Filled" : "true",
-        "w"      : 0.6,
-        "h"      : 0.6,
-        "Layer"  : 0,
-        "Color"  : "#EFAD06"
-    }
-
-    return portrayal
-
-
-def destination() :
-    return {
-        "Shape"  : "circle",
-        "Filled" : "true",
-        "r"      : 0.8,
-        "Layer"  : 0,
-        "Color"  : "#EF767A"
-    }
-
-
-chart = ChartModule([{"Label" : "Number of carrying agents",
+chart = ChartModule([{"Label" : "Carrying",
                       "Color" : "Black"},
-                     {"Label" : "Remaining boxes",
+                     {"Label" : "Boxes",
                       "Color" : "#EFAD06"}],
                     data_collector_name = "data_collectors")
 
@@ -66,7 +26,7 @@ server = ModularServer(BoxModel,
                        [grid, chart],
                        "Box Carrying Model",
                        {"nb_agents" : 2,
-                        "nb_boxes" : 2,
+                        "nb_boxes" : 3,
                         "width" : 10,
                         "height" : 10,
                         "torus" : False})
