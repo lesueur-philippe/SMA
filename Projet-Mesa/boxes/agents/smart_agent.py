310b7dbfc66e0ac68a02d5301cbec05d2a872e98
Lesueur Philippe <lesueur@b11p13.fil.univ-lille1.fr>
2021-12-08 17:31:21 +0100

    Reworked negociations

--- Projet-Mesa/boxes/agents/smart_agent.py
+++ Projet-Mesa/boxes/agents/smart_agent.py
@@ -25,41 +25,8 @@ class SmartAgent(BaseAgent) :
         self.negociated_with = []
         self.wished_positions = []
         self.round = 0
-
-    def _choose_action(self) -> None :
-        pass
-
-    def __negociate(self) -> Position :
-        if not self.negociated:
-            self.available_next_steps()
-        
-        agents = []
-        for pos in self.wished_positions:
-            cellmates = self.model.grid.get_cell_list_contents(self.model.grid.get_neighborhood(
-                    pos, moore = False, include_center = True))
-            for agent in cellmates:
-                if isinstance(agent, SmartAgent) and (not agent == self) and not (agent in agents):
-                    agents.append(agent)
-        for agent in agents:
-            if not (agent in self.negociated_with):
-                self.model.solve_conflict(self, agent)
-        
-        if len(self.wished_positions) == 0:
-            return self.pos
-        return random.choice(self.wished_positions)
-            
-
-    def available_next_steps(self):
-        neighbouring_cells = self.model.grid.get_neighborhood(
-                self.pos, moore = False, include_center = False
-        )
-        for cell in neighbouring_cells.copy():
-            cellmates = self.model.grid.get_cell_list_contents(cell)
-            if len(cellmates) == 1:
-                if isinstance(cellmates[0], Box):
-                    neighbouring_cells.remove(cell)
-        self.wished_positions = neighbouring_cells
-        
+    
+    def get_wished_positions(self):
         if not self.carrying :
             target_position = self.target_pos
         else :
@@ -72,10 +39,80 @@ class SmartAgent(BaseAgent) :
             for distance in distances:
                 if distance[1] < distance_from_current :
                     wished.append(distance[0])
-            self.wished_positions = wished
-            if len(self.wished_positions) == 0:
-                self.wished_positions = neighbouring_cells
-                self.wished_positions.append(self.pos)
+            return wished
+        else :
+            return self.wished_positions
+
+    def __choose_action(self) -> None :
+
+        if not self.negociated:
+            self.available_next_steps()
+        
+        wished = self.get_wished_positions()
+
+        agents = []
+        for pos in self.wished_positions:
+            cellmates = self.model.grid.get_cell_list_contents(self.model.grid.get_neighborhood(
+                    pos, moore = False, include_center = True))
+            for agent in cellmates:
+                if isinstance(agent, SmartAgent) and (not agent == self) and not (agent in agents):
+                    agents.append(agent)
+        for agent in agents:
+            if not (agent in self.negociated_with):
+                self.__negociate(self, agent, wished)
+
+    def __negociate(self, agent, wished_positions) -> Position :
+        if not agent.negociated and agent.round == self.round:
+            agent.available_next_steps()
+        agent_wished_positions = agent..get_wished_positions()
+        if len(agent_wished_positions) > 1:
+            pos = random.choice(wished_positions)
+            if pos in agent.wished_positions:
+                agent.wished_positions.remove(pos)
+            return pos
+        elif len(agent_wished_positions) == 1:
+            if agent.wished_positions > 1:
+                for pos in wished_positions:
+                    if not pos in agent_wished_positions:
+                        try:
+                            agent.wished_positions.remove(pos)
+                        except ValueError:
+                            pass
+                    return pos
+                pos = random.choice(wished_positions)
+                try:
+                    agent.wished_positions.remove(pos)
+                except ValueError:
+                    pass
+                return pos
+            elif len(agent.wished_positions) == 1:
+                if wished_positions[0] == agent_wished_positions[0]:
+                    pass
+                else:
+                    try:
+                        agent.wished_positions.remove(wished_positions[0])
+                    except ValueError:
+                        pass
+                    return wished_positions[0]
+            else:
+                
+        else:
+
+        
+            
+
+    def available_next_steps(self):
+        neighbouring_cells = self.model.grid.get_neighborhood(
+                self.pos, moore = False, include_center = False
+        )
+        for cell in neighbouring_cells.copy():
+            cellmates = self.model.grid.get_cell_list_contents(cell)
+            if len(cellmates) == 1:
+                if isinstance(cellmates[0], Box) or (isinstance(cellmates[0], SmartAgent) and cellmates[0].round != self.round):
+                    neighbouring_cells.remove(cell)
+        self.wished_positions = neighbouring_cells
+        
+        
             
 
     def _pickup_box(self, box : Box) -> None :
